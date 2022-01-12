f6b41f2a50dba7a4efb59e770c3c3d17be1826f4
Ayumi <ayumi.highroof@gmail.com>
2022-01-12 12:50:20 +0900

    Request data from URL on load

--- IyashiDogListTests/DogLoaderTests.swift
+++ IyashiDogListTests/DogLoaderTests.swift
@@ -17,19 +17,44 @@ import XCTest
  */
 
 class RemoteDogLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load(from url: URL) {
+        client.get(from: url)
+    }
 }
 
 class HTTPClient {
-    var requestedUrl: URL?
+    func get(from url: URL) {
+    }
+    
 }
 
 class DogLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-       
-        let client = HTTPClient()
-        let _ = RemoteDogLoader()
+        let client = HTTPClientSpy()
+        let _ = RemoteDogLoader(client: client)
         XCTAssertNil(client.requestedUrl)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "http://a-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteDogLoader(client: client)
+        sut.load(from: url)
+        XCTAssertNotNil(client.requestedUrl)
+    }
+}
+
+private class HTTPClientSpy: HTTPClient {
+    var requestedUrl: URL?
+    
+    override func get(from url: URL) {
+        self.requestedUrl = url
     }
 }
