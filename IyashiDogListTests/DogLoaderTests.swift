983d40d5a8fedfa8575132df6c73b7b6ee2119de
Ayumi <ayumi.highroof@gmail.com>
2022-01-12 12:21:04 +0900

    RemoteDogLoader does not request data from url on initialization

--- IyashiDogListTests/DogLoaderTests.swift
+++ IyashiDogListTests/DogLoaderTests.swift
@@ -6,5 +6,30 @@
 //
 
 import XCTest
+
+/** TODO list
+ - Load dogs from API
+ - If successful
+    - Displays dogs
+ - If failure
+    - Shows an error message
+ 
+ */
+
+class RemoteDogLoader {
+    
+}
+
+class HTTPClient {
+    var requestedUrl: URL?
+}
+
 class DogLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+       
+        let client = HTTPClient()
+        let _ = RemoteDogLoader()
+        XCTAssertNil(client.requestedUrl)
+    }
 }
