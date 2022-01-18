b7c5dd5864726207c7a1834ba447ea4bc9ce8a43
Ayumi <ayumi.highroof@gmail.com>
2022-01-18 21:22:16 +0900

    Create 'HTTPClientURLSessionTests'

---
+++ IyashiDogListTests/HTTPClientURLSessionTests.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPClientURLSessionTests.swift
+//  IyashiDogListTests
+//
+//  Created by 宇高あゆみ on 2022/01/17.
+//
+
+import XCTest
+import IyashiDogList
+
+class HTTPClientURLSessionTests: XCTestCase {
+
+}
+
