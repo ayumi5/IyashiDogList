25b0bf40d3da7b6c603abfef1f91a046d4294270
Ayumi <ayumi.highroof@gmail.com>
2022-01-19 18:08:14 +0900

    Create 'URLSessionStub' that inherits URLProtocol

--- IyashiDogListTests/HTTPClientURLSessionTests.swift
+++ IyashiDogListTests/HTTPClientURLSessionTests.swift
@@ -9,6 +9,22 @@ import XCTest
 import IyashiDogList
 
 class HTTPClientURLSessionTests: XCTestCase {
-
 }
 
+private class URLSessionStub: URLProtocol {
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {
+        
+    }
+}
