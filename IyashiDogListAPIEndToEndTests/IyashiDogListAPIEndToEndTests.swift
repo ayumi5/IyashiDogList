8862a1e2e16f6c1b83eac2888e8f0111dcd671b1
Ayumi <ayumi.highroof@gmail.com>
2022-02-21 11:33:43 +0900

    Add API end-to-end tests

---
+++ IyashiDogListAPIEndToEndTests/IyashiDogListAPIEndToEndTests.swift
@@ -0,0 +1,12 @@
+//
+//  IyashiDogListAPIEndToEndTests.swift
+//  IyashiDogListAPIEndToEndTests
+//
+//  Created by 宇高あゆみ on 2022/02/21.
+//
+
+import XCTest
+
+class IyashiDogListAPIEndToEndTests: XCTestCase {
+
+}
