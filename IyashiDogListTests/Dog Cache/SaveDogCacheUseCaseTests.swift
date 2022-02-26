5581afa2af577ba4dc66158584d605c35e782cda
Ayumi <ayumi.highroof@gmail.com>
2022-02-26 14:50:33 +0900

    Add a new test file "SaveDogCacheUseCaseTests"

---
+++ IyashiDogListTests/Dog Cache/SaveDogCacheUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  SaveDogCacheUseCaseTests.swift
+//  IyashiDogListTests
+//
+//  Created by 宇高あゆみ on 2022/02/26.
+//
+
+import XCTest
+
+class SaveDogCacheUseCaseTests: XCTestCase {
+    
+}
