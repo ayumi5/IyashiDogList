cb5ff34c527049bf8e24ba20e87455889cae4882
Ayumi <ayumi.highroof@gmail.com>
2022-03-03 16:27:41 +0900

    Retrieving twice from empty cache delivers same empty results

--- IyashiDogListTests/Dog Cache/CoreDataDogStoreTests.swift
+++ IyashiDogListTests/Dog Cache/CoreDataDogStoreTests.swift
@@ -36,5 +36,23 @@ class CoreDataDogStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CoreDataDogStore()
+        
+        let exp = expectation(description: "Wait for retrieval completion")
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty): break
+                default:
+                    XCTFail("Expected two empty results, got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 
 }
