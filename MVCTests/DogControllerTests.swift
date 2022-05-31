4527f7aa113c201b8e3cb59fed242add6f85b97c
Ayumi <ayumi.highroof@gmail.com>
2022-05-31 10:23:50 +0900

    Show loading indicator on refresh

--- MVCTests/DogControllerTests.swift
+++ MVCTests/DogControllerTests.swift
@@ -77,6 +77,13 @@ final class DogControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
+    func test_pullToRefresh_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: DogViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
