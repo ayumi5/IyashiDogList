38800af8d44da96dfa0371bf31511fa40d040ee1
Ayumi <ayumi.highroof@gmail.com>
2022-06-02 12:40:28 +0900

    Extract UIButton test helpers to a separate file

---
+++ MVCTests/UIButton+TestHelpers.swift
@@ -0,0 +1,18 @@
+//
+//  UIButton+TestHelpers.swift
+//  MVCTests
+//
+//  Created by 宇高あゆみ on 2022/06/02.
+//
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
